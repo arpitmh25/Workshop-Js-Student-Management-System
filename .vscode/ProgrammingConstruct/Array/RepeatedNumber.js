66b1b786b7975e9758c6ff3cd95ef9eca42bf1f3
Arpit <arpitmh23@gmail.com>
2022-02-03 14:39:43 +0530

    Creating Regex pattern to check valid pin code

--- .vscode/ProgrammingConstruct/Array/RepeatedNumber.js
+++
@@ -1,8 +0,0 @@
-let repeaters = new Array()
-for(let  number  = 0 ; number <= 100 ; number ++)
-{
-    if (number%11 == 0 && number !=0) {
-        repeaters.push(number)
-    }
-}
-console.log("The reapeated numbers are "+repeaters);
